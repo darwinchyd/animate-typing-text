1a2f0b4364e955f7ab6b5e9e6ab2aae812d45f55
dwncy <darwin301194@gmail.com>
2017-04-24 00:30:52 +0700

    minify js browser

--- browser/bin/build.js
+++ browser/bin/build.js
@@ -2,14 +2,15 @@
 
 const fs = require('fs')
 const path = require('path')
-const uglify = require('uglify-js')
+const UglifyJS = require('uglify-js')
 
 const file = fs.readFileSync(path.join(__dirname, '../../index.js'), 'utf8')
 const noExports = file.split('\n').slice(0, -2).join('\n')
 const bound = noExports + `
-  (function() {
-    this.AnimateTypingText = AnimateTypingText
-  })()
+(function() {
+  this.AnimateTypingText = AnimateTypingText
+})()
 `
+const result = UglifyJS.minify(bound, { fromString: true })
 
-fs.writeFileSync(path.join(__dirname, '../dist/animate-typing-text.min.js'), bound)
+fs.writeFileSync(path.join(__dirname, '../dist/animate-typing-text.min.js'), result.code)
